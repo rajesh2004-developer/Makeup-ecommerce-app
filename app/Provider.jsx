4446e6121c08a5b3fa391595a95f59cf96da7832
rajesh2004-developer <iamrajesh65892004@gmail.com>
2025-10-04 16:41:52 +0530

    Navbar UI created

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -4,10 +4,12 @@ import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { AppSidebar } from './_components/Sidebar';
+import Navbar from './_components/Navbar';
 
 export function ThemeProvider({ children, ...props }) {
   return (
     <NextThemesProvider {...props}>
+      <Navbar />
       <SidebarProvider>
         <AppSidebar />
         <main>
